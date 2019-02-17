96a4b81ca7cb6eb711d51dbdc08ffd4ae4f410ec
Silvio Manuel <dr.silvio.manuel@gmail.com>
2019-02-17 23:13:26 +0200

    initial implementation of CircularSuffixArray class

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -1,23 +1,83 @@
+import edu.princeton.cs.algs4.StdOut;
 
 
 public class CircularSuffixArray {
+
+    private final static int R = 256;
+    private final char[] origin;
+    private final int cntChars;
+    private final Suffix[] suffixes;
+
     // circular suffix array of s
     public CircularSuffixArray(String s) {
-        throw new RuntimeException("Not implemented");
+        origin = s.toCharArray();
+        cntChars = s.length();
+        suffixes = new Suffix[cntChars];
+        for (int i = 0; i < cntChars; i++) suffixes[i] = new Suffix(i);
+        sort();
     }
 
     // length of s
     public int length() {
-        throw new RuntimeException("Not implemented");
+        return cntChars;
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        throw new RuntimeException("Not implemented");
+        return suffixes[i].shift;
     }
 
     // unit testing (required)
     public static void main(String[] args) {
-        throw new RuntimeException("Not implemented");
+        String word = args[0];
+        CircularSuffixArray circularSuffixArray = new CircularSuffixArray(word);
+        for (Suffix suffix: circularSuffixArray.suffixes) {
+            StdOut.println(suffix);
+        }
+        StdOut.println("length: " + circularSuffixArray.length());
+        for (int i = 0; i < circularSuffixArray.length(); i++) {
+            StdOut.println("index[" + i + "]: " + circularSuffixArray.index(i));
+        }
+    }
+
+    private void sort() {
+        Suffix[] aux = new Suffix[suffixes.length];
+        for (int ch = cntChars - 1; ch >= 0; ch--) {
+            int[] cnt = new int[R + 1];
+
+            for (Suffix suffix : suffixes) cnt[suffix.charAt(ch) + 1]++;
+
+            for (int r = 0; r < R; r++) cnt[r + 1] += cnt[r];
+
+            for (Suffix suffix : suffixes) aux[cnt[suffix.charAt(ch)]++] = suffix;
+
+            System.arraycopy(aux, 0, suffixes, 0, suffixes.length);
+        }
+    }
+
+    private class Suffix {
+
+        final int shift;
+        int index;
+
+        Suffix(int s) {
+            shift = s;
+            index = 0;
+        }
+
+        char charAt(int i) {
+            int pointer = shift + i;
+            int j = pointer < cntChars
+                    ? pointer
+                    : pointer - cntChars;
+            return origin[j];
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < cntChars; i++) sb.append(charAt(i));
+            return sb.toString();
+        }
     }
 }
