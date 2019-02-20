5a0489542f5f54e116e9b06c59ca0d407a8a4c3a
Silvio Manuel <dr.silvio.manuel@gmail.com>
2019-02-20 08:05:37 +0200

    Fix for CircularSuffixArray performance LSD -> Quick3

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class CircularSuffixArray {
 
-    private final int R = 256;
+    private final int CUTOFF =  15;
     private final String origin;
     private final int cntChars;
     private final Suffix[] suffixes;
@@ -18,7 +18,7 @@ public class CircularSuffixArray {
 
         for (int i = 0; i < cntChars; i++) suffixes[i] = new Suffix(i);
 
-        sortLsd();
+        sortQuick3();
     }
 
     // length of s
@@ -45,21 +45,64 @@ public class CircularSuffixArray {
         }
     }
 
-    private void sortLsd() {
-        Suffix[] aux = new Suffix[suffixes.length];
-        for (int ch = cntChars - 1; ch >= 0; ch--) {
-            int[] cnt = new int[R + 1];
-
-            for (Suffix suffix : suffixes) cnt[suffix.charAt(ch) + 1]++;
-
-            for (int r = 0; r < R; r++) cnt[r + 1] += cnt[r];
-
-            for (Suffix suffix : suffixes) aux[cnt[suffix.charAt(ch)]++] = suffix;
-
-            System.arraycopy(aux, 0, suffixes, 0, suffixes.length);
-        }
+    private void sortQuick3() {
+        sort(suffixes, 0, suffixes.length - 1, 0);
     }
 
+    // return the dth character of s, -1 if d = length of s
+    private static int charAt(Suffix s, int d) {
+        assert d >= 0 && d <= s.length();
+        if (d == s.length()) return -1;
+        return s.charAt(d);
+    }
+
+    // 3-way string quicksort a[lo..hi] starting at dth character
+    private void sort(Suffix[] a, int lo, int hi, int d) {
+
+        // cutoff to insertion sort for small subarrays
+        if (hi <= lo + CUTOFF) {
+            insertion(a, lo, hi, d);
+            return;
+        }
+
+        int lt = lo, gt = hi;
+        int v = charAt(a[lo], d);
+        int i = lo + 1;
+        while (i <= gt) {
+            int t = charAt(a[i], d);
+            if (t < v) exch(a, lt++, i++);
+            else if (t > v) exch(a, i, gt--);
+            else i++;
+        }
+
+        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
+        sort(a, lo, lt - 1, d);
+        if (v >= 0) sort(a, lt, gt, d + 1);
+        sort(a, gt + 1, hi, d);
+    }
+
+    // sort from a[lo] to a[hi], starting at the dth character
+    private static void insertion(Suffix[] a, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++)
+            for (int j = i; j > lo && less(a[j], a[j-1], d); j--)
+                exch(a, j, j-1);
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Suffix[] a, int i, int j) {
+        Suffix temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    // is v less than w, starting at character d
+    private static boolean less(Suffix v, Suffix w, int d) {
+        for (int i = d; i < Math.min(v.length(), w.length()); i++) {
+            if (v.charAt(i) < w.charAt(i)) return true;
+            if (v.charAt(i) > w.charAt(i)) return false;
+        }
+        return v.length() < w.length();
+    }
 
     private class Suffix {
 
@@ -75,6 +118,10 @@ public class CircularSuffixArray {
                     ? pointer
                     : pointer - cntChars;
             return origin.charAt(j);
+        }
+
+        int length() {
+            return origin.length();
         }
 
         @Override
